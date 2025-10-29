eb22619a9089dc2db047f2064459aaec63595986
alexvalera1978 <146354180+alexvalera1978@users.noreply.github.com>
2025-10-29 13:18:33 +0100

    Update facedetection.js

--- js/facedetection.js
+++ js/facedetection.js
@@ -2,7 +2,7 @@
 export class FaceDetector {
     constructor() {
         this.modelsLoaded = false;
-        this.modelPath = 'https://justadudewhohacks.github.io/face-api.js/models/';
+        this.modelPath = 'https://cdn.jsdelivr.net/npm/@vladmandic/face-api/model/';
     }
 
     async loadModels() {
