c3fe059413acd2630fdac929817678ee60899a7e
alexvalera1978 <146354180+alexvalera1978@users.noreply.github.com>
2025-10-29 12:31:06 +0100

    Update motiondetection.js

--- js/motiondetection.js
+++ js/motiondetection.js
@@ -1,6 +1,6 @@
 // Detección de movimiento
 export class MotionDetector {
-    constructor(videoElement, threshold = 30, minPixels = 8000) {
+    constructor(videoElement, threshold = 30, minPixels = 2000) {
         this.video = videoElement;
         this.threshold = threshold;
         this.minPixels = minPixels;
@@ -50,4 +50,5 @@ export class MotionDetector {
     }
 
 }
+
 
