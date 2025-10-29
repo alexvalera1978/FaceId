5bbccb73b37170e654fb980bd86d64e371332020
alexvalera1978 <146354180+alexvalera1978@users.noreply.github.com>
2025-10-29 13:17:55 +0100

    Update MotionDetector parameters for sensitivity

--- js/motiondetection.js
+++ js/motiondetection.js
@@ -1,6 +1,6 @@
 // Detección de movimiento
 export class MotionDetector {
-    constructor(videoElement, threshold = 30, minPixels = 2000) {
+    constructor(videoElement, threshold = 60, minPixels = 15000) {
         this.video = videoElement;
         this.threshold = threshold;
         this.minPixels = minPixels;
